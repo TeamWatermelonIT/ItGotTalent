18006063ba7815fc2792e35600cf2a5580483070
Daniel Petrov <danielpetrov222@gmail.com>
2016-03-09 01:06:19 +0200

    moduls, migrations, seeds
    1/3

---
+++ ItGotTalents/app/Http/routes.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+//Route::group(['middleware' => ['web']], function () {
+//    //
+//});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+    Route::group(
+        ['prefix' => 'api'],
+        function() {
+            Route::get('/', 'HomeController@index');
+            Route::get('projects/{id}', 'ProjectsController@show');
+        });
+
+
+});
+Route::group(
+    ['prefix' => 'api'],
+    function() {
+        Route::get('/', 'HomeController@index');
+        Route::get('students', 'StudentsController@index');
+        Route::get('students/{id}', 'StudentsController@show');
+        Route::get('students/{id}/projects', 'ProjectsController@showByStudentId');
+        Route::get('projects', 'ProjectsController@index');
+        Route::get('projects/{id}', 'ProjectsController@show');
+    });
