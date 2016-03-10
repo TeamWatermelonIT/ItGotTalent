b0c32dbb3dcb60b99c29da2eb9aa08e3b42f395e
Daniel Petrov <danielpetrov222@gmail.com>
2016-03-10 14:00:22 +0200

    v .0.1.1

---
+++ ItGotTalents/resources/views/index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<html ng-app="app">
+<head>
+    <link rel="stylesheet" href="/css/vendor.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+<md-button class="md-raised md-primary">Welcome to Angular Material</md-button>
+
+<script src="/js/vendor.js"></script>
+<script src="/js/app.js"></script>
+</body>
+</html>
